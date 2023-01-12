b679f6e56af1f345a431b7278a6401b2dbd33246
Xavier Garcia Moya <reiax98@protonmail.com>
2023-01-12 20:42:57 +0100

    Filtro de busqueda pero falta case insesitive

--- Bot/src/main.c
+++ Bot/src/main.c
@@ -6,18 +6,67 @@
 #include <conio.h>
 #include <stdbool.h>
 #include "rlutil.h"
-#define MAXWORDS 
-#define MAXCHARS 
+#define MAXWORDS 5
+#define MAXCHARS 128
+
 int main(){
 	SetConsoleOutputCP(1252);
     SetConsoleCP(1252);
 	srand(time(NULL)); 
+	static const char keywords[MAXWORDS][MAXCHARS] = {"exit","Putin","btc","iphone","game"};
+	char cadena[MAXCHARS]="";
+	char *word, *Words;
+	int i=0,length=0,lengthKey=0,resul=0;
+	bool find = false;
 
-	static const char paraulesClau[MAXWORDS][MAXCHARS] = {"exit","Putin","btc"}; 
 
 	printf("\tWelcome to the future my friend\n");
 	printf("\t\tEduBot is Here\n");
-	printf("------------------------------------------------");
+	printf("------------------------------------------------\n\n");
+
+	printf("Introduce una de estas palabras: exit, Putin, btc, iphone, game\n\n");
+
+	printf("[USER]:\t");
+	fgets(cadena, MAXCHARS, stdin);
+	cadena[strcspn(cadena, "\n")] = 0; //Para limpiar el buffer
+
+	word = strtok(cadena, " "); //? Fragmento la frase del usuario
+	while (word != NULL) { //! Filtro para buscar una palabra en la frase de usuario
+		find = false;
+		i=0;
+		while (i <= MAXWORDS){ //* Bucle para compara palabras
+			length = strlen(word);
+			lengthKey = strlen(keywords[i]);
+			if (length == lengthKey && (strcmpi(keywords[i],word)==0 || strcmpi(keywords[i],word)<0 || strcmpi(keywords[i],word)>0))
+			{
+				if(strstr(word,keywords[i])!=NULL){
+					
+					printf("EEES");
+					find = true;
+					// break;
+				}
+			}
+			
+			
+			i++;
+		}
+		if (find){break;}
+		word = strtok(NULL, " ");
+	}
+	if(find)
+	{
+		printf("Se encontró\n");
+	}else{
+		printf("No se encontró\n");
+	}
+	
+
+	printf("\n\nFin\n\n");
+
+	for ( i = 0; i < 3; i++)
+	{
+		
+	}
 
 	getch();
 	return 0;
@@ -30,7 +79,13 @@ int main(){
 // strlen(cad) - Cuenta
 // strcat(cad1,cad2) - Concatena
 // strcpy(cad1,cad2) - Machaca
-// strncpy(cad1,cad2,longitud) - Machaca controlando el tamaÃ±o de la palabra
+// strncpy(cad1,cad2,longitud) - Machaca controlando el tamaño de la palabra
 // strcmp(cad1,cad2) - Compara (Case Sensitive)
 // strcmpi(cad1,cad2) - Compara (Case Insesitive)
 // strstr(cad1,cad2) - Busca dentro de la cadena principal
+// strtok(cad1, dilimitador) - Sirve para separar las palabras. 
+// Ej: word = strtok(cadena," ");
+// while (word != NULL) {
+//     printf("%s\n", word);
+//     word = strtok(NULL, " ");
+// }
