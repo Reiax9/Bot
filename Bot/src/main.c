87a7df595900a6fdf43b0ef866cec4d4a21ef5ac
Xavi García <reiax98@protonmail.com>
2023-01-12 16:10:04 +0100

    Añadiendo un par de cosas...

--- Bot/src/main.c
+++ Bot/src/main.c
@@ -6,12 +6,14 @@
 #include <conio.h>
 #include <stdbool.h>
 #include "rlutil.h"
-
+#define MAXWORDS 
+#define MAXCHARS 
 int main(){
 	SetConsoleOutputCP(1252);
-	SetConsoleCP(1252);
-	
-	srand(time(NULL));
+    SetConsoleCP(1252);
+	srand(time(NULL)); 
+
+	static const char paraulesClau[MAXWORDS][MAXCHARS] = {"exit","Putin","btc"}; 
 
 	printf("\tWelcome to the future my friend\n");
 	printf("\t\tEduBot is Here\n");
@@ -20,3 +22,15 @@ int main(){
 	getch();
 	return 0;
 }
+
+// fgets(cadena,llargada,stdin);
+// cadena[strcspn(cadena, "\n")] = 0; Para limpiar el buffer
+
+
+// strlen(cad) - Cuenta
+// strcat(cad1,cad2) - Concatena
+// strcpy(cad1,cad2) - Machaca
+// strncpy(cad1,cad2,longitud) - Machaca controlando el tamaño de la palabra
+// strcmp(cad1,cad2) - Compara (Case Sensitive)
+// strcmpi(cad1,cad2) - Compara (Case Insesitive)
+// strstr(cad1,cad2) - Busca dentro de la cadena principal
